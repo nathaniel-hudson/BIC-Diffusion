40c04be7d428f6313135c9062e81365403a95116
Nathaniel Hudson <nchudson88@gmail.com>
2020-08-31 17:44:07 -0400

    Save

--- src/Algorithms/Proposed.py
+++ src/Algorithms/Proposed.py
@@ -1,10 +1,6 @@
 import networkx as nx
 import numpy as np
 
-def revised_solution(model, n_seeds):
-    ## gain(pp) +- loss(1 - pp)
-    return set()
-
 def opinion_degree_solution(model, n_seeds):
     """Proposed algorithm that ranks nodes for seeding by weighting the degree of each
        node by its some retractive measure of "net gain" in total opinion. Considerations
@@ -60,103 +56,6 @@ def opinion_degree_solution(model, n_seeds):
             delta[neighbor] -= opinion_impact * behavio_impact
 
     return S
-
-
-def my_floydw(model):
-    """Modified version of the Floyd-Warshall algorithm, that finds the "likeliest" paths.
-    
-    Parameters
-    ----------
-    model : BIC_Model
-        Instance of BIC model with OSN topology and other data.
-    
-    Returns
-    -------
-    np.array
-        2D matrix containing probability for v to be activated as a result of node u.
-    """
-    dist = np.zeros((len(model.graph), len(model.graph)))
-    if nx.is_directed(model.graph):
-        for (u,v) in model.graph.edges():
-            dist[u][v] = model.prop_prob(u, v, use_attempts=False)
-    else:
-        for (u,v) in model.graph.edges():
-            dist[u][v] = model.prop_prob(u, v, use_attempts=False)
-            dist[u][v] = model.prop_prob(v, u, use_attempts=False)
-    
-    for v in model.graph.nodes():
-        dist[v][v] = 1.0
-    
-    for k in model.graph.nodes():
-        for i in model.graph.nodes():
-            for j in model.graph.nodes():
-                if dist[i][j] < dist[i][k] * dist[k][j]:
-                    dist[i][j] = dist[i][k] * dist[k][j]
-    return dist
-
-
-def opinion_path_solution(model, n_seeds):
-    """An older proposed algorithm for seeding nodes by computing paths of probability for activation.
-
-    Parameters
-    ----------
-    model : BIC_Model
-        Instance of BIC model.
-    n_seeds : int
-        Number of nodes to seed.
-
-    Returns
-    -------
-    set
-        Nodes selected to be seeds.
-    """
-    V = model.graph.nodes()
-    dist = my_floydw(model)
-    net_gain = [[1 - model.init_opinion[v]] for v in V]
-    S = set()
-    option = 1
-
-    cpp_matrix = np.asarray(dist)
-    sum_matrix = np.array([[sum(cpp_matrix[i])] for i in range(len(cpp_matrix))])
-    net_gain = np.asarray(net_gain)
-    T = [1 for v in range(len(V))]
-
-    # ORIGINAL:
-    # R = np.matmul(cpp_matrix, net_gain)
-
-    best_fit = np.dot(cpp_matrix, sum_matrix) * net_gain
-
-    for i in range(n_seeds):
-        # Grab the most "influential" node based on the (delta * phi) computation.
-        arg = np.argmax(best_fit)
-        S = S.union({arg})
-
-        ### NEW VERSION.
-        # NOTE: I need to find a way to consider the in- and out-neighbors of `arg`.
-        for j in range(len(best_fit)):
-            cpp_matrix[arg][j] = cpp_matrix[j][arg] = 0.0
-
-        best_fit = np.dot(cpp_matrix, sum_matrix) * net_gain
-
-        '''
-        This part needs to be played around with... HOW do we update it?
-        '''
-        '''
-        ### Original version.
-        for neighbor in model.graph.neighbors(arg):
-            T[neighbor] += 1
-
-        # "Delete" the influence the `arg`-th row and colulmn in `R`.
-        delta[arg, :] = 0  # Fill the arg-th row in delta with 0.
-        delta[:, arg] = 0  # Fill the arg-th col in delta with 0.
-        R = np.matmul(delta, phi)
-
-        # Create the neighborhood discount matrix.
-        _T = np.array([[1/t] for t in T])
-        R = R * _T
-        '''
-
-    return list(S)
 
 
 def proto1_solution(model, n_seeds, theta=0.2):
@@ -219,7 +118,7 @@ def proto1_solution(model, n_seeds, theta=0.2):
 
 
 
-def new_solution(model, n_seeds, max_iter=2, theta=0.0001):
+def new_solution_bad(model, n_seeds, max_iter=2, theta=0.0001):
     
     model.prepare()
 
@@ -263,13 +162,13 @@ def new_solution(model, n_seeds, max_iter=2, theta=0.0001):
         for u in model.graph.nodes():
             k_out = nx.degree(model.graph, u)
             for v in model.graph.neighbors(u):
-                w1 = 1.0 / k_out
+                pp_vu = 1.0 / k_out
                 pp_vu = model.prop_prob(v, u, use_attempts=False)
                 pp_uv = model.prop_prob(u, v, use_attempts=False)
                 cand1 = p_arr[v][i - 1]
                 cand2 = p_arr[u][i - 2]
-                # if (cand1 > theta) and (cand1 - w1 * cand2 > theta):
-                #     p_arr[u][i] = p_arr[u][i] + pp_uv * (cand1 - w1 * cand2) + \
+                # if (cand1 > theta) and (cand1 - pp_vu * cand2 > theta):
+                #     p_arr[u][i] = p_arr[u][i] + pp_uv * (cand1 - pp_vu * cand2) + \
                 if (cand1 > theta) and (cand1 - pp_vu * cand2 > theta):
                     adv = p_arr[u][i] + pp_uv * (cand1 - pp_vu * cand2)
                     pen = (1-pp_uv) * (model.penalized_update(v, 0) - model.init_opinion[v])
@@ -321,13 +220,13 @@ def new_solution(model, n_seeds, max_iter=2, theta=0.0001):
         for u in model.graph.nodes():
             k_out = nx.degree(model.graph, u)
             for v in model.graph.neighbors(u):
-                w1 = 1.0 / k_out
-                w2 = model.prop_prob(u, v, use_attempts=False)
-                potential1 = p_arr[v][iteration - 1]
-                potential2 = p_arr[u][iteration - 2]
-                if (potential1 > theta) and (potential1 - w1 * potential2 > theta):
-                    p_arr[u][iteration] = p_arr[u][iteration] + w2 * \
-                        (potential1 - w1 * potential2)
+                pp_vu = 1.0 / k_out
+                pp_uv = model.prop_prob(u, v, use_attempts=False)
+                cand1 = p_arr[v][iteration - 1]
+                cand2 = p_arr[u][iteration - 2]
+                if (cand1 > theta) and (cand1 - pp_vu * cand2 > theta):
+                    p_arr[u][iteration] = p_arr[u][iteration] + pp_uv * \
+                        (cand1 - pp_vu * cand2)
             p_arr[u][max_iter] = p_arr[u][max_iter] + p_arr[u][iteration]
         iteration += 1
 
@@ -356,22 +255,21 @@ def __fast_LAIM(model, n_seeds, pruned_nodes, max_iter=2, theta=0.0001):
     p_arr = [[0 for j in range(max_iter + 1)] for i in model.graph.nodes()]
     for node in model.graph.nodes():
         if node in pruned_nodes:
-            p_arr[node][1] = 1
+            p_arr[node][1] = (1 - model.init_opinion[node]) ** 2 #1
         else:
-            p_arr[node][1] = 0 # NOTE: We may want to make this negative? Just an idea.
+            p_arr[node][1] = 0#-1 # NOTE: We may want to make this negative? Just an idea.
 
     iteration = 2
     while (iteration < max_iter):
         for u in model.graph.nodes():
             k_out = nx.degree(model.graph, u)
             for v in model.graph.neighbors(u):
-                w1 = 1.0 / k_out
-                w2 = model.prop_prob(u, v, use_attempts=False)
-                potential1 = p_arr[v][iteration - 1]
-                potential2 = p_arr[u][iteration - 2]
-                if (potential1 > theta) and (potential1 - w1 * potential2 > theta):
-                    p_arr[u][iteration] = p_arr[u][iteration] + w2 * \
-                        (potential1 - w1 * potential2)
+                pp_uv = model.prop_prob(u, v, use_attempts=False)
+                pp_vu = model.prop_prob(v, u, use_attempts=False) # 1.0 / k_out
+                cand1 = p_arr[v][iteration - 1]
+                cand2 = p_arr[u][iteration - 2]
+                if (cand1 > theta) and (cand1 - pp_vu * cand2 > theta):
+                    p_arr[u][iteration] = p_arr[u][iteration] + pp_uv * (cand1 - pp_vu * cand2)
             p_arr[u][max_iter] = p_arr[u][max_iter] + p_arr[u][iteration]
         iteration += 1
 
@@ -391,20 +289,7 @@ def __fast_LAIM(model, n_seeds, pruned_nodes, max_iter=2, theta=0.0001):
     return seeds
 
 
-def nom_solution(model, n_seeds, psi=0.4):
+def new_solution(model, n_seeds, psi=0.5):
     pruned_nodes = set([u for u in model.graph.nodes() if model.init_opinion[u] <= psi])
-    # pn_set = set(pruned_nodes) ## We do this to simplify lookup for below.
-    # pruned_edges = [(u,v) for (u,v) in model.graph.edges() if (u in pn_set) and (v in pn_set)]
-    # mapping_from = {idx: pruned_nodes[idx] for idx in range(len(pruned_nodes))}
-    # mapping_to   = {pruned_nodes[idx]: idx for idx in range(len(pruned_nodes))}
-
-    # graph_psi = nx.DiGraph() if model.graph.is_directed() else nx.Graph()
-    # graph_psi.add_nodes_from(pruned_nodes)
-    # graph_psi.add_edges_from(pruned_edges)
-
-    # graph_psi = nx.relabel_nodes(graph_psi, mapping_to)
-    # opinion = [model.init_opinion[node] for node in pruned_nodes]
-    # ffm = {node: model.ffm[node] for node in pruned_nodes}
-    # model_psi = BIC(graph_psi, ffm, opinion)
-
-    return __fast_LAIM(model, n_seeds, pruned_nodes)
+    seeds = __fast_LAIM(model, n_seeds, pruned_nodes)
+    return seeds
