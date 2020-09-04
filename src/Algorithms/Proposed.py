2c1181b7504cf6e90c4cef93143b804a0908a2e7
Nathaniel Hudson <nchudson88@gmail.com>
2020-09-03 21:22:34 -0400

    Update Proposed.py

--- src/Algorithms/Proposed.py
+++ src/Algorithms/Proposed.py
@@ -58,14 +58,55 @@ def opinion_degree_solution(model, n_seeds):
     return S
 
 
-def __fast_LAIM(model, n_seeds, influencers, influencees, psi, max_iter=2, theta=0.0001):
+def __approx_influence(model, seeded, influencers, influencees, psi, inf_opinion_avg, max_iter=2, theta=0.0001):
+    node_set = set(model.graph.nodes()) - seeded
+    max_iter += 1
+    influence = [[0 for j in range(max_iter + 1)] for i in model.graph.nodes()]
+    cost = [[0 for j in range(max_iter + 1)] for i in model.graph.nodes()]
+    for node in node_set:
+        influence[node][1] = (1 - model.init_opinion[node])
+        cost[node][1] = (inf_opinion_avg[node] - model.init_opinion[node])
+
+    for i in range(max_iter):
+        for u in node_set:
+            for v in influencees[u]:
+                pp_uv = model.prop_prob(u, v, use_attempts=False)
+                pp_vu = model.prop_prob(v, u, use_attempts=False) 
+                influence[u][i] += pp_uv * (influence[v][i-1] - pp_vu * influence[u][i-2])
+                cost[u][i] += (1-pp_uv) * (cost[v][i-1] - (1-pp_vu) * cost[u][i-2]) ## NOTE: This is bad...
+            influence[u][max_iter] += influence[u][i]
+            cost[u][max_iter] += cost[u][i]
+
+    seed = None
+    max_val = float("-inf")
+    for cand in node_set:
+        val = influence[cand][max_iter] + cost[cand][max_iter]
+        if val > max_val:
+            seed = cand
+            max_val = val
+    return seed
+
+
+def __LAIM(model, n_seeds, influencers, influencees, psi, inf_opinion_avg, max_iter=2, theta=0.0001):
+    seeds = set()
+    node_set = set(model.graph.nodes())
+    for _ in range(n_seeds):
+        new_seed = __approx_influence(model, seeds, influencers, influencees, psi, inf_opinion_avg, max_iter, theta)
+        seeds.add(new_seed)
+    return seeds
+
+
+
+def __fast_LAIM(model, n_seeds, influencers, influencees, psi, inf_opinion_avg, max_iter=2, theta=0.0001):
     if n_seeds < 1:
         return set()
 
     max_iter += 1
     influence = [[0 for j in range(max_iter + 1)] for i in model.graph.nodes()]
+    cost = [[0 for j in range(max_iter + 1)] for i in model.graph.nodes()]
     for node in model.graph.nodes():
         influence[node][1] = (1 - model.init_opinion[node])
+        cost[node][1] = (inf_opinion_avg[node] - model.init_opinion[node])
 
     for i in range(max_iter):
         for u in model.graph.nodes():
@@ -73,8 +114,9 @@ def __fast_LAIM(model, n_seeds, influencers, influencees, psi, max_iter=2, theta
                 pp_uv = model.prop_prob(u, v, use_attempts=False)
                 pp_vu = model.prop_prob(v, u, use_attempts=False) 
                 influence[u][i] += pp_uv * (influence[v][i-1] - pp_vu * influence[u][i-2])
-                # influence[u][i] -= (1 - pp_uv) * (1 - psi[v])
+                cost[u][i] += (1-pp_uv) * (cost[v][i-1] - (1-pp_vu) * cost[u][i-2]) ## NOTE: This is bad...
             influence[u][max_iter] += influence[u][i]
+            cost[u][max_iter] += cost[u][i]
 
     seeds = set()
     node_set = set(model.graph.nodes())
@@ -96,10 +138,16 @@ def __fast_LAIM(model, n_seeds, influencers, influencees, psi, max_iter=2, theta
             #     new_seed = candidate
             #     temp_influencees = cand_influencees
 
-            ## NOTE: Penalty-free version.
-            if influence[candidate][max_iter] > max_val:
-                max_val  = influence[candidate][max_iter] 
+            ## NOTE: Influence * Cost approach.
+            val = influence[candidate][max_iter] + cost[candidate][max_iter]
+            if val > max_val:
+                max_val = val
                 new_seed = candidate
+
+            ## NOTE: Penalty-free version.
+            # if influence[candidate][max_iter] > max_val:
+            #     max_val  = influence[candidate][max_iter] 
+            #     new_seed = candidate
         seeds.add(new_seed)
         covered_influencees = covered_influencees.union(temp_influencees)
         
@@ -129,12 +177,14 @@ def new_solution(model, n_seeds, p=0.01, theta=1.0):
     influencers = {node: set(get_in_neighbors(node)) for node in graph.nodes()}
     influencees = {node: set(graph.neighbors(node))  for node in graph.nodes()}
     inf_opinion_std = {}
+    inf_opinion_avg = {}
     for node in graph.nodes():
         # if len(influencers[node]) == 0:
         #     inf_opinion_std[node] = 0
         #     continue
         opinions = [opinion[influencer] for influencer in influencers[node]]
         inf_opinion_std[node] = np.std(opinions + [opinion[node]])
+        inf_opinion_avg[node] = np.mean(opinions + [opinion[node]])
     
     def general_influencers(v):
         min_val = opinion[v] - inf_opinion_std[v]
@@ -160,4 +210,5 @@ def new_solution(model, n_seeds, p=0.01, theta=1.0):
         else:
             resilience[node] = len(gen_influencers) / len(pen_influencers)
 
-    return __fast_LAIM(model, n_seeds, influencers, influencees, resilience)
+    return __LAIM(model, n_seeds, influencers, influencees, resilience, inf_opinion_avg)
+    # return __fast_LAIM(model, n_seeds, influencers, influencees, resilience, inf_opinion_avg)
