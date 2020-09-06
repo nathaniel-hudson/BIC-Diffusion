3b24cb51d4106af6837b03192fb24458ea425b4c
Nathaniel Hudson <nchudson88@gmail.com>
2020-09-06 02:18:29 -0400

    Checkpoint

--- src/Algorithms/Proposed.py
+++ src/Algorithms/Proposed.py
@@ -220,23 +220,8 @@ def new_solution(model, n_seeds, max_iters=2):
     node_set = set(graph.nodes())
     opinion = model.init_opinion.copy()
 
-
     in_neighbors = graph.neighbors if not nx.is_directed(graph) else graph.predecessors
     out_neighbors = graph.neighbors
-
-
-    def gen_influencers(node):
-        influencers = set()
-        in_neighbors_opinions = [opinion[x] for x in in_neighbors(node)]
-        in_neighbors_opinions.append(opinion[node])
-        std = np.std(in_neighbors_opinions)
-
-        for in_nbor in in_neighbors(node):
-            if opinion[node]-std <= opinion[in_nbor] and opinion[in_nbor] <= opinion[node]+std:
-                influencers.add(in_nbor)
-        
-        return influencers
-
 
     def pen_influencers(node):
         influencers = set()
@@ -249,71 +234,65 @@ def new_solution(model, n_seeds, max_iters=2):
                 influencers.add(in_nbor)
         
         return influencers
-
-
-    in_neighborhood_avg_opinion = {
-        node: np.mean([opinion[neighbor] for neighbor in in_neighbors(node)] + [opinion[node]])
-        for node in node_set
-    }
     
     impact = np.zeros(shape=(max_iters+1, len(node_set), len(node_set)))
-    penalty = np.zeros(shape=(max_iters+1, len(node_set), len(node_set)))
-    for u in node_set:
-        impact[1][u][u] = (1.0 - opinion[u])
+    penalty = np.zeros(shape=(len(node_set), len(node_set)))
+    for v in node_set:
+        impact[1][v][v] = 1.0 - opinion[v]
         
-        temp = [opinion[v] - opinion[u] for v in pen_influencers(u)]
-        penalty[1][u][u] = 0 if len(temp) == 0 else np.mean(temp)
-        # penalty[1][u][u] = (in_neighborhood_avg_opinion[u] - opinion[u])
+        influencers = pen_influencers(v)
+        for u in influencers:
+            diff = (opinion[u] - opinion[v]) / len(influencers)
+            penalty[u][v] += diff
+            penalty[v][v] += diff
 
     for itr in range(2, max_iters):
         for u in node_set:
-            for v in graph.neighbors(u):
+            for v in out_neighbors(u):
                 pp_uv = model.prop_prob(u, v, use_attempts=False)
                 pp_vu = model.prop_prob(v, u, use_attempts=False)
+
                 impact[itr][u][v] += pp_uv * \
                     (impact[itr-1][v][v] - pp_vu * impact[itr-2][u][u])
                 impact[itr][u][u] += impact[itr][u][v]
 
                 ## NOTE: Experimental.
-                penalty[itr][u][v] += (1-pp_uv) * \
-                    penalty[itr][v][v]
-                    # (penalty[itr][v][v] - (1-pp_vu) * penalty[itr-2][u][u])
-                penalty[itr][u][u] += impact[itr][u][v]
-
+                # penalty[itr][u][v] += (1-pp_uv) * \
+                #     penalty[itr][v][v]
+                #     # (penalty[itr][v][v] - (1-pp_vu) * penalty[itr-2][u][u])
+                # penalty[itr][u][u] += impact[itr][u][v]
 
             impact[max_iters][u][u] += impact[itr][u][u]
+            # impact[max_iters][u][u] = impact[1][u][u] + impact[max_iters][u][u] + impact[itr][u][u]
 
             ## NOTE: Experimental.
-            penalty[max_iters][u][u] += penalty[itr][u][u]
+            # penalty[max_iters][u][u] = penalty[1][u][u] + penalty[max_iters][u][u] + penalty[itr][u][u]
             
 
-    impact_D  = impact[max_iters].diagonal()
-    penalty_D = impact[max_iters].diagonal()
     seed_set = set()
     for k in range(n_seeds):
-        u = None
+        v = None
         max_val = float("-inf")
-        for candidate in node_set - seed_set:
-            # val = impact_D[candidate] * penalty_D[candidate]
-            val = impact_D[candidate] + penalty[max_iters][candidate][candidate]
-            if val > max_val:
-                u = candidate
-                max_val = val
-        seed_set.add(u)
 
-        opinion[u] = 1.0
+        for cand in node_set - seed_set:
+            val = impact[max_iters][cand][cand] + penalty[cand][cand]
+            if val > max_val:
+                v = cand
+                max_val = val
+        seed_set.add(v)
 
         # Anneal the `impact` matrix based on the newly seeded node.
-        for t in in_neighbors(u):
-            impact[max_iters][t][t]  -= impact[max_iters-1][t][u]
-
-            ## NOTE: Experimental.
-            # penalty[max_iters][t][t] -= penalty[max_iters-1][t][u]
-            # temp = [opinion[tt] - opinion[t] for tt in pen_influencers(t)]
-            # penalty[1][t][t] = 0 if len(temp) == 0 else np.mean(temp)
-            # penalty[max_iters][t][t] -= penalty[max_iters-1][t][u]
-
-        impact[max_iters][u][u] = impact[max_iters-1][u][u] = float("-inf")#0
-        penalty[max_iters][u][u] = penalty[max_iters-1][u][u] = 1#0
+        for u in in_neighbors(v):
+            impact[max_iters][u][u] -= impact[max_iters-1][u][v]
+            impact[max_iters][u][u] -= impact[max_iters-1][v][u]
+            
+        opinion[v] = 1.0
+        penalty = np.zeros(shape=(len(node_set), len(node_set)))
+        for z in node_set:
+            influencers = pen_influencers(z)
+            for y in influencers:
+                diff = (opinion[y] - opinion[z]) / len(influencers)
+                penalty[y][z] += diff
+                penalty[z][z] += diff
 
     return seed_set
